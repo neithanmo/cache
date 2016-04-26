911b1b70a458499d5872b9a2c94136af43fa313a
Natanael Mojica <neithanmo@gmail.com>
2016-04-25 20:43:35 -0600

    cache

--- cache.h
+++ cache.h
@@ -2,4 +2,21 @@
 #define CACHE
 
 #endif // CACHE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+class cache
+{
+    public:
+        cache();
+        ~cache();
+
+    private:
+
+
+
+};
 
